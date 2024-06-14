d01bc22be840b26311ff30e410bbdbf3bac3cf7a
橘朔也 <167387830+dadhiya-nazan@users.noreply.github.com>
2024-06-14 13:38:46 +0900

    Initial commit

---
+++ fukuv0614/Form1.cs
@@ -0,0 +1,34 @@
+namespace fukuv0614
+{
+    public partial class Form1 : Form
+    {
+        static Random random = new Random();
+        int ans = random.Next(51);
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show ($"{count}‰ñ–Ú‚Ì’§í");
+            int yosou;
+            yosou = int.Parse(TextBox.Text);
+
+            if(yosou == ans)
+            {
+                MessageBox.Show("Bingo!!");
+            }
+            else if(yosou > ans)
+            {
+                MessageBox.Show("less");
+            }
+            else
+            {
+                MessageBox.Show("Greater");
+            }
+        }
+    }
+}
